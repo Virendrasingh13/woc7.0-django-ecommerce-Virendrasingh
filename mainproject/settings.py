8b3b9688a68e83a6f847ef0b7f1d0b85b9b4d2bc
Virendrasingh <161146998+Virendrasingh13@users.noreply.github.com>
2025-02-24 20:01:43 +0530

    Update settings.py

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -9,11 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
-# from pathlib import Path
-# INSTALLED_APPS.insert(1, 'whitenoise.runserver_nostatic')  
-# MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')  
-
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
