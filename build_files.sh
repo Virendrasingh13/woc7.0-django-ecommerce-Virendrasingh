7aaf5cd61573303d017407b70154a2e7c9e40d60
Virendrasingh <161146998+Virendrasingh13@users.noreply.github.com>
2025-02-24 19:57:23 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,18 @@
+#!/bin/bash
+
 echo "BUILD START"
-python 3.9 -m pip install requirements.txt
-python 3.9 manage.py collectstatic --noinput --clear
+
+# Install dependencies correctly
+python3.9 -m pip install -r requirements.txt
+
+# Run Django collectstatic
+python3.9 manage.py collectstatic --noinput --clear
+
+# Apply migrations (if your project needs a database)
+python3.9 manage.py migrate
+
+# Create the correct build directory
+mkdir -p staticfiles_build
+cp -r static/* staticfiles_build/
+
 echo "BUILD END"
